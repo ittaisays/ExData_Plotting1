95a43227090d68a10b1ae8c7c3d1215104cc4e69
ittaisays <ittai.barzilay@gmail.com>
2014-05-11 00:48:24 -0700

    stringsAsFactors set to false, updated images to reflect new code

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
-setwd("~/Box Sync/Coursera/ExploratoryData/ExData_Plotting1/") #Set working directory
-data<-read.table(file="household_power_consumption.txt",header=TRUE,sep=";") #Read in Data
+setwd("~/Box Sync/Coursera/ExploratoryData/Project1/") #Set working directory
+data<-read.table(file="household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE) #Read in Data
 lines <- grep('^[1-2]/2/2007', readLines('household_power_consumption.txt')) #Identify lines within our date range and create a vector of the row numbers we want
 subsetData<-data[lines,] #Create the data frame with the targeted dates
 
